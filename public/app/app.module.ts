e78308b840c3d6b225118223ca5dad7463d649b5
Manmeet Gupta <manmeet.gupta@mobisoftinfotech.com>
2016-10-03 19:57:15 +0530

    login page

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent }   from "./components/app/app.component";
 import {LoginComponent} from "./components/login/login.component";
 
--- public/app/components/login/login.component.ts
+++ public/app/components/login/login.component.ts
@@ -41,6 +41,6 @@ export class LoginComponent {
             validateUserName : {
                 valid : false
             }
-        }
+        };
     }
 }
