dce3131c482cdc5d02ded5d200b8997576becf39
Manmeet Gupta <manmeet.gupta@mobisoftinfotech.com>
2016-10-05 12:08:51 +0530

    postinstall hook updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require("gulp");
 var tslint = require("gulp-tslint");
 
+var typescript = require('gulp-tsc');
+ 
+gulp.task('tsc', function(){
+  gulp.src(["public/**/*.ts", "server.ts","app/**/*.ts"])
+    .pipe(typescript({
+        "target": "es5",
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "sourceMap": true,
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "removeComments": false,
+        "noImplicitAny": false,
+        "exclude": [
+            "node_modules",
+            "**/*.d.ts"
+        ]
+}))
+    .pipe(gulp.dest('.'))
+});
+
 gulp.task("tslint", () =>
    gulp.src(["public/**/*.ts", "server.ts","app/**/*.ts"])
     .pipe(tslint({
