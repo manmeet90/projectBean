89440e74174b1b543baaef4e2397b8a3d91d6e4b
Manmeet Gupta <manmeet.gupta@mobisoftinfotech.com>
2016-10-06 18:48:15 +0530

    postmark added to project

--- public/app/projectdetail.module.js
+++ public/app/projectdetail.module.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
+var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var common_1 = require("@angular/common");
 var forms_1 = require("@angular/forms");
